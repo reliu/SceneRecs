57d48bcaf7d7119c6ac5dd566594f03e17894c9d
reliu <reliu@calpoly.edu>
2017-11-15 13:39:08 -0800

    Button - top 3 recs

--- EinsteinVisionExample/ViewController.swift
+++ EinsteinVisionExample/ViewController.swift
@@ -14,7 +14,13 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     let travelScenes = ["Airplane interior", "Airport check-in", "Airport exterior", "Airport terminal",
                         "Highway", "Hotel lobby", "Hotel room", "Bus or train interior"]
     let swimScenes = ["Beach or shoreline", "Pool", "Natural water"]
+    
     //var webView = UIWebView()
+    
+    @IBOutlet weak var rec1: UIButton!
+    @IBOutlet weak var rec2: UIButton!
+    @IBOutlet weak var rec3: UIButton!
+    
     @IBOutlet weak var webView: UIWebView!
     
     @IBOutlet weak var imageView: UIImageView!
@@ -23,7 +29,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBOutlet weak var analysisText: UITextView!
     
     @IBAction func takeAndAnalyzePhoto(_ sender: UIButton) {
-        
+
         let image = UIImagePickerController()
         image.delegate = self
         
@@ -45,6 +51,11 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
+        self.analysisText.text = ""
+        self.rec1.setTitle("", for: .normal)
+        self.rec2.setTitle("", for: .normal)
+        self.rec3.setTitle("", for: .normal)
+        
         if let chosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = chosenImage
             
@@ -53,28 +64,38 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             let imageStr = imageData.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
             
             // Initialize the service with a valid access token - CHANGE THIS OFTEN
-            let service = PredictionService(bearerToken: "IVHH6KEIDDH7WBWZ5UR7UBMBBAXBWRVZW5PAXCWYJGEV6RRL7KHRCNA26635AJ2W26MJ42RKRKT5ROIUNXCU442BRXYGLORQVW7WHMI")
+            let service = PredictionService(bearerToken: "6KG2LMRATPILFKCPHMROGZD4D46XWMBH2HNYDXV25HYQ2EQP5IPKDMHHV6XKBBOABW2S3HGLFP6PEEWYGH7IDSCAL52FD5UVSB2QYYI")
 
             // Upload base64 for prediction on the Scene Classifier Model
             service?.predictBase64(modelId: "SceneClassifier", base64: imageStr, sampleId: "", completion: { (result) in
                 var resultString = ""
+                var probLabels = [String]()
                 
                 if (result != nil) {
-                    resultString = ((result?.probabilities!)?[0].label!)! // Get scene with highest probability
-                    print(resultString)
-                    self.handleScene(resultString: resultString)
+//                    resultString = ((result?.probabilities!)?[0].label!)! // Get scene with highest probability
+//                    self.handleScene(resultString: resultString)
                     //UIApplication.shared.open(URL(string: "http://www.rei.com")!) //opens safari
                     //self.webView.loadRequest(URLRequest(url: URL(string: "http://www.rei.com")!)) //opens in-app
-//                    for prob in (result?.probabilities!)! {
-//                        resultString = resultString + prob.label! + " (" + String(prob.probability!) + ")\n"
-//                    }
+                    if (result?.probabilities!)!.count < 3 {
+                        for prob in (result?.probabilities!)! {
+                            //resultString = resultString + prob.label! + " (" + String(prob.probability!) + ")\n"
+                            probLabels.append(prob.label!)
+                        }
+                    }
+                    else {
+                        for i in 0 ..< 3 {
+                            let prob = (result?.probabilities!)![i]
+                            //resultString = resultString + prob.label! + " (" + String(prob.probability!) + ")\n"
+                            probLabels.append(prob.label!)
+                        }
+                    }
+                    self.generateRecs(probLabels: probLabels)
                 } else {
                     resultString = "No data found"
                 }
-                
-                //self.analysisText.text = resultString
+//                print(resultString)
+//                self.analysisText.text = resultString
             })
-
         }
         else {
             print("Something went wrong")
@@ -84,12 +105,70 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         
     }
     
+    func generateRecs(probLabels: [String]) {
+        let probLabels = Array(Set(probLabels)) //make into a unique array
+        var output = [String]()
+        
+        // generate rec output
+        for label in probLabels {
+            if travelScenes.contains(label) && !output.contains("travel clothing") && output.count <= 3 {
+                output.append("travel clothing")
+            }
+            else if swimScenes.contains(label) && !output.contains("swimwear") && output.count <= 3 {
+                output.append("paddling clothing")
+                output.append("swimwear")
+            }
+        }
+        
+        // assign output text to respective button
+        if output.count == 1 {
+            self.rec1.setTitle(output[0], for: .normal)
+            self.rec1.addTarget(self, action: #selector(action(sender:)), for: UIControlEvents.touchUpInside)
+        }
+        else if output.count == 2 {
+            self.rec1.setTitle(output[0], for: .normal)
+            self.rec1.addTarget(self, action: #selector(action(sender:)), for: UIControlEvents.touchUpInside)
+            self.rec2.setTitle(output[1], for: .normal)
+            self.rec2.addTarget(self, action: #selector(action(sender:)), for: UIControlEvents.touchUpInside)
+        }
+        else if output.count == 3 {
+            self.rec1.setTitle(output[0], for: .normal)
+            self.rec1.addTarget(self, action: #selector(action(sender:)), for: UIControlEvents.touchUpInside)
+            self.rec2.setTitle(output[1], for: .normal)
+            self.rec2.addTarget(self, action: #selector(action(sender:)), for: UIControlEvents.touchUpInside)
+            self.rec3.setTitle(output[2], for: .normal)
+            self.rec3.addTarget(self, action: #selector(action(sender:)), for: UIControlEvents.touchUpInside)
+        }
+        else {
+            self.analysisText.text = "Error: no recommendations for this photo"
+        }
+        
+        //print(output)
+    }
+    
+    func action(sender:UIButton!) {
+        print("Button Clicked")
+        if sender.titleLabel?.text == "travel clothing" {
+            UIApplication.shared.open(URL(string: "https://www.rei.com/s/travel-clothing")!) //opens safari
+        }
+        else if sender.titleLabel?.text == "paddling clothing" {
+            UIApplication.shared.open(URL(string: "https://www.rei.com/c/paddling-clothing")!) //opens safari
+        }
+        else if sender.titleLabel?.text == "swimwear" {
+            UIApplication.shared.open(URL(string: "https://www.rei.com/c/swimwear")!) //opens safari
+        }
+        else {
+            print("Could not parse button text")
+        }
+        
+    }
+    
     func handleScene(resultString: String) {
         if travelScenes.contains(resultString) {
             UIApplication.shared.open(URL(string: "https://www.rei.com/s/travel-clothing")!) //opens safari
         }
         else if swimScenes.contains(resultString) {
-            UIApplication.shared.open(URL(string: "https://www.rei.com/c/womens-swimwear")!) //opens safari
+            UIApplication.shared.open(URL(string: "https://www.rei.com/c/swimwear")!) //opens safari
         }
         else {
             self.analysisText.text = "Error: could not recognize scene."
