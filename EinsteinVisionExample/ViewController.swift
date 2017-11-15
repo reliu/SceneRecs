c83491e2c91147bd60f3cb39085b3e7578597edd
reliu <reliu@calpoly.edu>
2017-11-14 23:59:17 -0800

    open safari for travel & swim scenes

--- EinsteinVisionExample/ViewController.swift
+++ EinsteinVisionExample/ViewController.swift
@@ -11,9 +11,14 @@ import SalesforceEinsteinVision
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
+    let travelScenes = ["Airplane interior", "Airport check-in", "Airport exterior", "Airport terminal",
+                        "Highway", "Hotel lobby", "Hotel room", "Bus or train interior"]
+    let swimScenes = ["Beach or shoreline", "Pool", "Natural water"]
+    //var webView = UIWebView()
+    @IBOutlet weak var webView: UIWebView!
+    
     @IBOutlet weak var imageView: UIImageView!
     var imagePicker: UIImagePickerController!
-    
 
     @IBOutlet weak var analysisText: UITextView!
     
@@ -48,7 +53,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             let imageStr = imageData.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
             
             // Initialize the service with a valid access token - CHANGE THIS OFTEN
-            let service = PredictionService(bearerToken: "")
+            let service = PredictionService(bearerToken: "IVHH6KEIDDH7WBWZ5UR7UBMBBAXBWRVZW5PAXCWYJGEV6RRL7KHRCNA26635AJ2W26MJ42RKRKT5ROIUNXCU442BRXYGLORQVW7WHMI")
 
             // Upload base64 for prediction on the Scene Classifier Model
             service?.predictBase64(modelId: "SceneClassifier", base64: imageStr, sampleId: "", completion: { (result) in
@@ -57,6 +62,9 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
                 if (result != nil) {
                     resultString = ((result?.probabilities!)?[0].label!)! // Get scene with highest probability
                     print(resultString)
+                    self.handleScene(resultString: resultString)
+                    //UIApplication.shared.open(URL(string: "http://www.rei.com")!) //opens safari
+                    //self.webView.loadRequest(URLRequest(url: URL(string: "http://www.rei.com")!)) //opens in-app
 //                    for prob in (result?.probabilities!)! {
 //                        resultString = resultString + prob.label! + " (" + String(prob.probability!) + ")\n"
 //                    }
@@ -64,7 +72,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
                     resultString = "No data found"
                 }
                 
-                self.analysisText.text = resultString
+                //self.analysisText.text = resultString
             })
 
         }
@@ -76,11 +84,22 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         
     }
     
+    func handleScene(resultString: String) {
+        if travelScenes.contains(resultString) {
+            UIApplication.shared.open(URL(string: "https://www.rei.com/s/travel-clothing")!) //opens safari
+        }
+        else if swimScenes.contains(resultString) {
+            UIApplication.shared.open(URL(string: "https://www.rei.com/c/womens-swimwear")!) //opens safari
+        }
+        else {
+            self.analysisText.text = "Error: could not recognize scene."
+        }
+    }
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
